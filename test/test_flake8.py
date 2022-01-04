35f7364f9b64370282f37aabd8924ba3d9b8c788
Ilja Orlovs <vrghost@gmail.com>
2022-01-04 12:52:26 +0000

    tests are passing now

--- test/test_flake8.py
+++ test/test_flake8.py
@@ -23,10 +23,7 @@ class TestIgnores:
     def example(self, request):
         """Create a test file."""
         testdir = request.getfixturevalue("testdir")
-        import sys
-
-        print(testdir, file=sys.stderr)
-        p = testdir.makepyfile("")
+        p = testdir.makepyfile("example")
         p.write("class AClass:\n    pass\n       \n\n# too many spaces")
         return p
 
