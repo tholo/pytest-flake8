7ce7ce1f4026d36673f4b8db8a0631fb7f2eb882
Actions <github+actions@gmail.com>
2022-01-04 14:40:06 +0000

    Bump version: 1.0.8 → 1.0.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="pytest-flake8-v2",
-    version="1.0.8",
+    version="1.0.9",
     description="pytest plugin to check FLAKE8 requirements",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
--- src/pytest_flake8.py
+++ src/pytest_flake8.py
@@ -11,7 +11,7 @@ import pytest
 from flake8.main import application
 from flake8.options import config
 
-__version__ = "1.0.8"
+__version__ = "1.0.9"
 
 HISTKEY = "flake8/mtimes"
 
